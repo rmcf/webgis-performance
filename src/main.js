cbdafe05af642c3c0292b802591c70fb69182be9
avm <rmcf@ukr.net>
2020-10-12 00:02:03 +0300

    added library vuelayers

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
+import VueLayers from 'vuelayers'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
+import 'vuelayers/lib/style.css'
 
 Vue.use(VueMaterial)
+Vue.use(VueLayers)
 
 new Vue({
   render: h => h(App),
