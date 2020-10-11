911427d70f0237dd1ccb8c4b2dfafdcd4ee3781e
avm <rmcf@ukr.net>
2020-10-12 00:04:47 +0300

    added library vuelayers

---
+++ src/components/Map.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="map">
+    <vl-map
+      :load-tiles-while-animating="true"
+      :load-tiles-while-interacting="true"
+      data-projection="EPSG:4326"
+    >
+      <vl-view :zoom.sync="zoom" :center.sync="center"></vl-view>
+
+      <vl-layer-tile id="osm">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+    </vl-map>
+
+    <div class="inform">
+      Zoom: {{ zoom }}<br />
+      Center: {{ center }}<br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map",
+  data() {
+    return {
+      zoom: 7,
+      center: [25.371622547063208, 58.602373945187594],
+    };
+  },
+};
+</script>
+
+<style scoped>
+div.map {
+  height: 70vh;
+}
+
+div.inform {
+  margin-top: 1em;
+  padding: 1em;
+  border: 2px solid #448aff;
+}
+</style>
