109cf583d3ad877487d907d1994eb9f647a97742
avm <rmcf@ukr.net>
2020-10-13 15:31:39 +0300

    minor changes

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm run build
 git add .
 echo "Write comment: "
 read comment
-git commit -m "'$comment'"
+git commit -m "$comment"
 git push -u origin master
 git subtree push --prefix dist origin gh-pages
 
