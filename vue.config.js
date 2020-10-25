7a401ea40e413a63e313c7f07ad3c8b8237ef541
avm <rmcf@ukr.net>
2020-10-26 00:43:28 +0200

    fighting against CORS error

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '/soilpygeoapi': {
-                target: 'https://soil-pygeoapi-docker-bozea3cspa-ew.a.run.app/',
-                pathRewrite: { '^/soilpygeoapi': '' }
-
-            },
-            '/maerchenland': {
-                target: 'http://www.maerchenland-rostock.de/',
-                pathRewrite: { '^/maerchenland': '' }
-            }
-        }
-    }
+    publicPath: '/webgis-performance/'
 }
