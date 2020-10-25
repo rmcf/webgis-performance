6f16073858d30f59b85306b6ea93c0f85ae4547f
avm <rmcf@ukr.net>
2020-10-26 01:06:24 +0200

    minor

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,6 @@ module.exports = {
             '/soilpygeoapi': {
                 target: 'https://soil-pygeoapi-docker-bozea3cspa-ew.a.run.app/',
                 pathRewrite: { '^/soilpygeoapi': '' }
-
             },
             '/maerchenland': {
                 target: 'http://www.maerchenland-rostock.de/',
