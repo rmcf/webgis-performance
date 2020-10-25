e7a036c0ce4adcebfb8aef6cbedf40c0aaf51ea0
avm <rmcf@ukr.net>
2020-10-26 01:19:39 +0200

    urls

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
 module.exports = {
     publicPath: '/webgis-performance/',
-    devServer: {
-        proxy: {
-            '/soilpygeoapi': {
-                target: 'https://soil-pygeoapi-docker-bozea3cspa-ew.a.run.app/',
-                pathRewrite: { '^/soilpygeoapi': '' }
-            },
-            '/maerchenland': {
-                target: 'http://www.maerchenland-rostock.de/',
-                pathRewrite: { '^/maerchenland': '' }
-            }
-        }
-    }
 }
