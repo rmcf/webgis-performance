3740a95b680dd9c9d9b27b91b09b51469834f639
avm <rmcf@ukr.net>
2021-01-03 21:43:28 +0200

    PRIA wms layer CORS

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
     publicPath: '/webgis-performance/',
     crossorigin: 'anonymous',
-    devServer: {
-        proxy: {
-            '/pria': {
-                target: 'https://kls.pria.ee/',
-                pathRewrite: { '^/pria': '' },
-                changeOrigin: true
-            }
-        }
-    }
 }
