19cb8221c01af834e0c7d870d9e27ccf582c8f52
avm <rmcf@ukr.net>
2021-01-03 21:41:23 +0200

    PRIA wms layer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     publicPath: '/webgis-performance/',
+    crossorigin: 'anonymous',
+    devServer: {
+        proxy: {
+            '/pria': {
+                target: 'https://kls.pria.ee/',
+                pathRewrite: { '^/pria': '' },
+                changeOrigin: true
+            }
+        }
+    }
 }
